fb18e1f2155cbf64e334b95a0871a5483b676cc1
jankupczyk <73750630+jankupczyk@users.noreply.github.com>
2021-06-29 11:13:52 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
-var copyoutput = () => {
+const copyoutput = () => {
     let name = document.getElementById("name").value;
     let item = document.getElementById("item").value;
-    let starred = document.getElementById("starred").checked ? "★" : "";
-    let paintkit = document.getElementById("paintkit").value;
+    let knfglv = document.getElementById("knfglv").checked ? "★" : "";
+    let paint = document.getElementById("paint").value;
     let stattrak = document.getElementById("stattrak").checked ? " StatTrak™" : "";
     let rarity = String.fromCharCode(eval(document.getElementById("rarity").value));
 
     let input = document.getElementById("end");
     input.value =
-        `playerradio Radio.WePlanted "\u2028\x03\x03${name} \x01has opened a container and found: ${rarity}${starred}${stattrak} ${item} | ${paintkit}"`;
+        `playerradio Radio.WePlanted "\u2028\x03\x03${name} \x01has opened a container and found: ${rarity}${knfglv}${stattrak} ${item} | ${paint}"`;
     input.select();
     input.setSelectionRange(0, input.value.length)
     document.execCommand("copy");
 };
 
-var VAC = () => {
+const VAC = () => {
     let name = document.getElementById("name").value;
 
     let VAC = document.getElementById("vacc");
@@ -25,7 +25,7 @@ var VAC = () => {
     document.execCommand("copy");
 };
 
-var resetoutput = () => {
+const resetoutput = () => {
     document.getElementById("end").value = '';
     document.getElementById("vacc").value = '';
 };
